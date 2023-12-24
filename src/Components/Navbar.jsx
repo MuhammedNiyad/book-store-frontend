ba3f37be704903df21ed5ab46e87c16bf8008098
Muhammed Niyad <niyadpunniyath007@gmail.com>
2023-12-25 00:15:35 +0530

    created complete Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,10 @@
 import { Link, NavLink} from 'react-router-dom';
 
 //Import Logo from assets...!
-import Logo from '../assets/logo.png';
+import Logo from '../assets/Book_brand_logo.png';
+
+//Import CSS....!
+import './Navbar.css';
 
 //Import NavLinks from Data..!
 import {navLinks, navRight} from '../Data/Data';
@@ -11,52 +14,78 @@ import {navLinks, navRight} from '../Data/Data';
 import { CiMenuFries } from "react-icons/ci";
 import { TfiClose } from "react-icons/tfi";
 
-//Import Button...!
-import Button from './Button';
+//Import State...!
+import { useState } from 'react';
+
 
 
 
 export default function Navbar() {
+    const [toggle, setToggle] = useState(false);
+
+// This for when scroll then side nav hide....!
+    if(innerWidth < 768){
+        window.addEventListener('scroll', ()=> {
+            document.querySelector('.nav-Links-Side').classList.add('hidden');
+            setToggle(false);
+        })
+    }
+
   return (
-    <nav>
-        <div>
+    <nav className={`nav h-24 grid place-items-center sticky top-0 bg-[#f3f2ec] z-50 border-1 border-[#838382] px-3 lg:h-[90px] ${toggle? 'shadow-lg': ''}`}>
+        <div className='container nav-container flex items-center justify-between lg:relative lg:h-full'>
             {/*......Logo......*/}
-            <Link to={'/'}>
-                <img className='w-10' src={Logo} alt='logo' />
+            <Link to={'/'} className='logo w-[120px] '>
+                <img src={Logo} alt='logo' />
             </Link>
             {/*......Nav-Links.......*/}
-            <ul>
+            <ul className='nav-links hidden md:flex items-center gap-7'>
                 {
                     navLinks.map(({name, path}, index)=> {
                         return(
-                            <li key={index}>
-                                <NavLink to={path}>{name}</NavLink>
+                            <li key={index} className='text-[#606060] text-base uppercase' >
+                                <NavLink to={path} className={({isActive})=> {
+                                    isActive ? 'text-[#c7b064]' : ''
+                                }}>{name}</NavLink>
                             </li>
                         )
                     })
                 }
             </ul>
             {/*......Nav-Rights.......*/}
-            <div className='nav-right'>
+            <div className='nav-right flex items-center gap-7  '>
                 {
                     navRight.managements.map((item, index)=>{
                         return(
                             <Link key={index} 
                             // target='_blank' 
-                            className='management-icons' to={item.link}>
+                            className='management-icons text-[#7a7a7a] text-[17px] ' to={item.link}>
                                 <item.icon />
                             </Link>
                         )
                     })
                 }
                 
-                <Button>
-                    <CiMenuFries />
-                    <TfiClose />
-                </Button>
-
+                <button onClick={()=> setToggle((togPrev)=> !togPrev)} className='menu-button flex md:hidden text-[#7a7a7a] text-lg'>
+                    {
+                        toggle? <TfiClose /> : <CiMenuFries />
+                    } 
+                </button>
+                {/* ......nav-links side bar...... */}
+            <ul className={`nav-Links-Side ${toggle? 'flex' : 'hidden' } flex-col gap-0 w-64 top-[90px] right-0 fixed`}>
+                    {
+                        navLinks.map(({name, path}, index)=>{
+                            return(
+                                <li key={index} className='text-[#606060] text-base uppercase hover:bg-[#f6ce4c]' >
+                                <NavLink to={path} className={({isActive})=> {
+                                    isActive ? 'text-[#c7b064]' : ''
+                                }}>{name}</NavLink>
+                            </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
-
         </div>
     </nav>
   )
