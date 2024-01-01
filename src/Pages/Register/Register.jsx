bbb556fadf8701b8560b9a3e3191203bff75053f
Muhammed Niyad <niyadpunniyath007@gmail.com>
2024-01-01 09:37:54 +0530

    Register page created

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,14 +1,17 @@
 // Import signin css....!
-import './SignIn.css';
+import './Register.css';
 
 // import react icons..........!
 import { LuUserCircle } from "react-icons/lu";
 import { MdLockOutline } from "react-icons/md";
+import { HiOutlineMail } from "react-icons/hi";
+
+// Import Link.........!
 import { Link } from 'react-router-dom';
 
 
 
-export default function SignIn() {
+export default function Register() {
   return (
     <section className="flex justify-center items-start w-full min-h-screen overflow-hidden">
         <div className='bg-[#edebe4] mt-32 py-20 px-16 md:px-36 relative'>
@@ -19,14 +22,19 @@ export default function SignIn() {
                     <input type='text' required className='h-16 text-xl w-[250px] sm:w=[300px] md:w-[350px]'/> 
                     <label>Username:</label>
                 </div>
+                <div className='input-box'>
+                    <span className='icon'><HiOutlineMail /></span>
+                    <input type='email' required className='h-16 text-xl w-[250px] sm:w=[300px] md:w-[350px]'/> 
+                    <label>Email:</label>
+                </div>
                     <div className='input-box'>
                     <span className='icon'><MdLockOutline /></span>
                     <input type='password' required className='h-16 text-xl w-[250px] sm:w=[300px] md:w-[350px]'/>
                         <label>Password:</label>
                     </div>
-                        <button type='submit' className="btn uppercase mt-3 py-3 px-9 duration-300 relative bg-transparent hover:text-white after:contents:'' after:w-0 after:h-full after:bg-black after:absolute after:left-0 after:top-0 after:duration-300 after:hover:w-full ">Sign In</button>
+                        <button type='submit' className="btn uppercase mt-3 py-3 px-9 duration-300 relative bg-transparent hover:text-white after:contents:'' after:w-0 after:h-full after:bg-black after:absolute after:left-0 after:top-0 after:duration-300 after:hover:w-full ">Register</button>
             </form>
-            <p className='absolute bottom-3 md:left-4'>Don't have an account? <Link to={'/register'}  className='text-lg hover:underline hover:text-amber-900'>Register</Link></p>
+            <p className='absolute bottom-3 md:left-4'>Have an account? <Link to={'/sign-in'}  className='text-lg hover:underline hover:text-amber-900'>Sign In</Link></p>
         </div>
     </section>
   )
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -10,6 +10,8 @@ import Footer from '../Components/Footer';
 import Book from '../Pages/Book';
 import Shope from '../Pages/Shope/Shope';
 import SignIn from '../Pages/Sign_In/SignIn';
+import Register from '../Pages/Register/Register';
+
 
 export default function Router() {
   return (
@@ -21,6 +23,7 @@ export default function Router() {
         <Route path='/book/:id' element={<Book />} />
         <Route path='/shope' element={<Shope />} />
         <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
       <Footer />
     </BrowserRouter>
